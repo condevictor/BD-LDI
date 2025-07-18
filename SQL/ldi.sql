2d15ab7a6691b25cde911c762cec16fb78f64a61
condevictor <victorconde@gmx.com>
2025-07-18 00:57:07 -0300

    add tables

--- SQL/ldi.sql
+++ SQL/ldi.sql
@@ -36,9 +36,35 @@ CREATE TYPE enum_forma_pagamento AS ENUM(
 	'Transferência Bancária',
 	'Dinheiro'
 );
+
 CREATE TABLE pagamento(
 	id_pagamento INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	valor_total NUMERIC(15, 2) NOT NULL,
 	forma_pagamento enum_forma_pagamento NOT NULL,
 	data_pagamento TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-); 
+);
+
+CREATE TABLE multa(
+	id_multa INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	valor_multa NUMERIC(15,2) NOT NULL
+);
+
+CREATE TABLE servico_extra(
+	id_servico INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	nome VARCHAR(255) NOT NULL,
+	descricao TEXT,
+	valor_servico NUMERIC(15,2) NOT NULL
+);
+
+CREATE TABLE estorno(
+	id_estorno INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	valor_estorno NUMERIC(15,2) NOT NULL,
+	data_estorno TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE cancelamento(
+	id_cancelamento INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	tipo_cancelamento VARCHAR(255),
+	data_cancelamento TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
